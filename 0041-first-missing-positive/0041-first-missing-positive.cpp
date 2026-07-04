cf6c24168265af55557bd521143fe9e238d54b63
Madhav Sharma <madhavsharmambx@gmail.com>
2026-07-04 23:04:57 +0530

    Time: 0 ms (100%), Space: 55.2 MB (31.73%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
@@ -12,6 +12,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (nums[i] != i + 1) return i + 1;
         }
+
         return n + 1;
     }
 };
